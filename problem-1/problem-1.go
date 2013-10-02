d3b1ce078634519a0b552c80786e7cb30c126382
William Oliveira <sudowilliam@gmail.com>
2013-10-02 01:51:17 -0300

    learning the syntax

--- problem-1/problem-1.go
+++ problem-1/problem-1.go
@@ -1,11 +1,16 @@
 package main
 
 import "fmt"
+import "math"
 
 // Learning Go!
 
+const str string = "constant"
+
 func main() {
 	// variables
+	fmt.Println("////// VARIABLES //////")
+
 	var s string = "initial"
 	fmt.Println(s)
 
@@ -20,4 +25,35 @@ func main() {
 
 	f := "short"
 	fmt.Println(f)
+
+	// constants
+	fmt.Println("////// CONSTANTS //////")
+
+	fmt.Println(str)
+
+	const n = 500000000
+
+	const d2 = 3e20 / n
+	fmt.Println(d2)
+
+	fmt.Println(int64(d2))
+
+	fmt.Println(math.Sin(n))
+
+	fmt.Println("////// LOOPS //////")
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
 }
