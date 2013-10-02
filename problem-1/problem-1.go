2511b1700fd4ff7acfbae090cc37df5a764be41a
William Oliveira <sudowilliam@gmail.com>
2013-10-02 01:44:29 -0300

    adding first go program

---
+++ problem-1/problem-1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Learning Go!
+
+func main() {
+	// variables
+	var s string = "initial"
+	fmt.Println(s)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
